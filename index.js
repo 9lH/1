e4d6e42e5accaa8a39ddadc1ec870f103aa2706b
Yan <9lH.LLlypyn@gmail.com>
2024-11-07 21:03:04 +0200

    counting letter

---
+++ index.js
@@ -0,0 +1,7 @@
+const s = 'You can also directly specify the project name and the template you want to use via additional command line options.'
+const counts = new Map
+
+for(letter of s) {
+  counts.set(letter, (counts.get(letter) ?? 0) + 1)
+ }
+console.log(counts)
