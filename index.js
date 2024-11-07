9fe16b2751af11086dc26e9ba2de66a035bd9de1
Yan <9lH.LLlypyn@gmail.com>
2024-11-08 01:17:47 +0200

    create getWords and tests

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const s = 'You can also directly specify the project name and the template you want to use via additional command line options.'
-const counts = new Map
+import getWords from "./lib/getWords.mjs"
+getWords("fdkskfpec")
 
-for(letter of s) {
-  counts.set(letter, (counts.get(letter) ?? 0) + 1)
- }
-console.log(counts)
+// const s = 'You can also directly specify the project name and the template you want to use via additional command line options.'
+// const counts = new Map
+
+// for(letter of s) {
+//   counts.set(letter, (counts.get(letter) ?? 0) + 1)
+//  }
+// console.log(counts)
